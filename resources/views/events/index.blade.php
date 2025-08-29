4f30207e987d0f2d37a470018c27af7f54f1d274
Josh <153073298+josh-yo@users.noreply.github.com>
2025-08-29 21:02:11 +1000

    style: apply Bootstrap CDN

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,19 +1,32 @@
-<h1>Upcoming Events</h1>
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Title</th>
-            <th>Date</th>
-            <th>Location</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($events as $event)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Upcoming Events</h1>
+    <table class="table table-striped">
+        <thead>
             <tr>
-                <td>{{ $event['title'] }}</td>
-                <td>{{ $event['date_time'] }}</td>
-                <td>{{ $event['location'] }}</td>
+                <th>Title</th>
+                <th>Date</th>
+                <th>Location</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach($events as $event)
+                <tr>
+                    <td>{{ $event['title'] }}</td>
+                    <td>{{ $event['date_time'] }}</td>
+                    <td>{{ $event['location'] }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
+
