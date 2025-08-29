442258900c7eae98ed93ea7f09e9140913c387d0
Josh <153073298+josh-yo@users.noreply.github.com>
2025-08-29 21:34:33 +1000

    feat: add initial Navbar component

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -7,6 +7,8 @@
     <title>Home Page</title>
 </head>
 <body>
+    <x-navbar />
+
     <h1>Upcoming Events</h1>
     <table class="table table-striped">
         <thead>
@@ -29,4 +31,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
-
