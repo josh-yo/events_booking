f1f4d1b5e5de802d5a0e9949fa8203977117b4c8
Josh <153073298+josh-yo@users.noreply.github.com>
2025-09-01 18:09:42 +1000

    feat: add links from index to detail page

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -6,14 +6,20 @@
         @foreach($events as $event)
             <div class="col-md-3 mb-3">
                 <div class="card h-100 shadow-sm">
-                    <img src="{{ $event->image_path }}" 
-                        onerror="this.onerror=null;this.src='{{ asset('images/default.jpg') }}';" 
-                        class="card-img-top" 
-                        style="height:200px;object-fit:cover;" 
-                        alt="{{ $event->title }}">
+                    <a href="{{ route('events.show', $event->id) }}">
+                        <img src="{{ $event->image_path }}" 
+                            onerror="this.onerror=null;this.src='{{ asset('images/default.jpg') }}';" 
+                            class="card-img-top" 
+                            style="height:200px;object-fit:cover;" 
+                            alt="{{ $event->title }}">
+                    </a>
 
                     <div class="card-body">
-                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <h3 class="card-title event_title">
+                            <a href="{{ route('events.show', $event->id) }}" class="text-decoration-none text-dark">
+                                {{ $event->title }}
+                            </a>
+                        </h3>
                         <p class="card-text date_time">{{ \Carbon\Carbon::parse($event->date_time)->format('d/M/Y, g:i A') }} </p>
                         <p class="card-text">{{ $event->location }}</p>
                         
